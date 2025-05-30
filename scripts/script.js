452099e05f66e1300703fd5c9fae932305d02052
Dejan Krajisnik <krajisnikdejan@gmail.com>
2025-05-30 12:38:13 +0200

    Added mobile navigation bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-document.querySelector('h1').addEventListener('click', () => {
+document.addEventListener('click', () => {
     console.log('Click')
+})
+const menuButton = document.querySelector('.menu_button');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeButton = document.querySelector('.close_button');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.add('menu-open')
+})
+
+closeButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('menu-open')
 })
