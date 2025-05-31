124a9504c4abba0e6814972efb5ca9c2751f6f15
Dejan Krajisnik <krajisnikdejan@gmail.com>
2025-05-31 16:23:07 +0200

    Added product info section

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
-document.addEventListener('click', () => {
-    console.log('Click')
-})
 const menuButton = document.querySelector('.menu_button');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeButton = document.querySelector('.close_button');
+const amountInput = document.querySelector('#amount_input')
 
 menuButton.addEventListener('click', () => {
     mobileMenu.classList.add('menu-open')
@@ -12,3 +10,21 @@ menuButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     mobileMenu.classList.remove('menu-open')
 })
+
+const checkNumber = (e) => {
+    if (!(e.key > 0 && e.key < 10)) {
+        return false;
+    }
+}
+
+const subOne = () => {
+    if (amountInput.value < 2) {
+        return;
+    } else {
+        amountInput.value -= 1;
+    }
+}
+
+const addOne = () => {
+    amountInput.value = +amountInput.value + 1;
+}
